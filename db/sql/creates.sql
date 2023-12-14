a70c3dd8ae43b87771f847ec2403046033a54de8
Juan Quintero <juanestquintero@gmail.com>
2023-12-14 17:06:34 -0500

    chore: add id autoincrement

--- db/sql/creates.sql
+++ db/sql/creates.sql
@@ -1,9 +1,8 @@
 -- table URLS shorted
 CREATE TABLE urls (
-  id INT NOT NULL,
-  title VARCHAR(100) NOT NULL,
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(100),
   address VARCHAR(500) NOT NULL,
-  shorted VARCHAR(50) NOT NULL UNIQUE,
-  clicks INT NOT NULL,
-  PRIMARY KEY (id)
+  shorted VARCHAR(50) UNIQUE,
+  clicks INT DEFAULT 0
 );
