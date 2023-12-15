1359cb77f04f1f235e67e4cf981f1481b9a9b65b
Juan Quintero <juanestquintero@gmail.com>
2023-12-14 19:33:51 -0500

    feat(api): get html page title (#6)

--- api/app/controllers/urls.py
+++ api/app/controllers/urls.py
@@ -1,6 +1,8 @@
 
 import string
-from asyncio import sleep
+import requests
+from bs4 import BeautifulSoup
+from sqlalchemy.orm import Session
 
 from app.services import urls as service
 
@@ -16,18 +18,32 @@ def generate_short_url(_id: int) -> str:
     return result or "0"
 
 
-async def get_page_title():
-    print("Getting html page title...", flush=True)
-    await sleep(5)
-    print("Done!", flush=True)
+async def get_page_title(db: Session, url: str):
+    try:
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            title_tag = soup.find('title')
+            if title_tag:
+                db_url = service.read_url(db, url)
+                db_url.title = title_tag.text.strip()
+                db.commit()
+                print(f"Fetched title! {db_url.title}", flush=True)
+            else:
+                print(f"No title tag found on the page: \n {url}", flush=True)
+        else:
+            print(f"Error fetch response on: \n {url}", flush=True)
 
-def create_shorten_url(db, request, error_logger, original_url):
+    except Exception as e:
+        print(f"Error: getting title tag {url} \n {str(e)}", flush=True)
+
+def create_shorten_url(db: Session, request, error_logger, original_url):
     # Insert row and get autoincrement id
     db_url = service.create_url(db, original_url)
     try:
         # Update the short URL with the generated one
         url_id = generate_short_url(db_url.id)
-        db_url.short_url = f'{request.base_url}{url_id}'
+        db_url.shorted = f'{request.base_url}{url_id}'
         db.commit()
         return db_url
     except Exception as e:
